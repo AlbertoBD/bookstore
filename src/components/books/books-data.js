c92029849da7c47521b2cbb58defd2e31fd2908a
AlbertoBD <albertobd956@gmail.com>
2022-05-31 03:21:34 +0300

    add cart

--- src/components/books/books-data.js
+++ src/components/books/books-data.js
@@ -7,7 +7,8 @@ const books = [
         description: "The Lord of the Rings is an epic high fantasy novel written by English author J. R. R. Tolkien. The story began as a sequel to Tolkien's 1937 fantasy novel The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, The Lord of the Rings is one of the best-selling novels ever written, with over 150 million copies sold",
         price: "119.99",
         image: "https://m.media-amazon.com/images/I/A1yy50fuVnL._AC_UY545_FMwebp_QL65_.jpg",
-        rating: 5
+        rating: 5,
+        stock: 10,
     },
     {
         id: 2,
--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,10 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CartContext from '../../context/cartContext'
+import { Badge, Dropdown, DropdownButton } from 'react-bootstrap'
+import { getBooks } from '../books/books-data';
 import "./cart.css"
 
 export default function Cart() {
     const { cart, addToCart } = useContext(CartContext);
+    const [quantity, setQuantity] = useState(1);
+    const book = getBooks()[0];
     return (
-        <div>cart</div>
+        <div className="cart_main">
+            <p className="cart_header">Cosul meu</p>
+            <div className="cart_content">
+                <div className="cart_photo">
+                    <img src={book.image} alt="product_image" className="cart_img"/>
+                </div>
+                <div className="cart_info">
+                    <p className="cart_title">{book.title}</p>
+                    <div className="cart_details">
+                        <DropdownButton id="dropdown-basic-button" title={quantity}>
+                            { //make an array from book stock 
+                                Array.from(Array(book.stock).keys()).map(number => {
+                                    return <Dropdown.Item onClick={() => setQuantity(number + 1)}>{number + 1}</Dropdown.Item>
+                                })
+                            }
+                        </DropdownButton>
+                        <Badge className="cart_price">{(book.price * quantity).toFixed(2)} lei</Badge>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
